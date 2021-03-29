bdddf327bc24739932ede59d66c2986ca01c46d2
Sergei <Sergeydolin@mail.ru>
2021-03-29 22:36:02 +0700

    ver 3.29.21

--- Moduls/ParamReader.py
+++ Moduls/ParamReader.py
@@ -10,7 +10,7 @@ def Param4():
         print("Error file")
 
 
-# (Point 1.2-3)
+# (Point 1.2)
 def rdk():
     i = 0
     kod = [12.390122175216670, 2.832145333290100, 20.112080812454220, 15.690995275974270,
@@ -31,11 +31,34 @@ def rdk():
     except IOError:
         print("Error file")
 
-
-def DecodeParam2():
+# (Point 1.3)
+def Param2():
     ff = rdk()
+
     ng = 250    # Harmonic number
     nput = int(100*len(ff)+0.5)
+    perut = np.zeros((nput, ng))
+    amput = np.zeros((nput, ng))
+    fazut = np.zeros((nput, ng))
+
+    perxp = np.zeros((nput, ng))
+    ampxp = np.zeros((nput, ng))
+    fazxp = np.zeros((nput, ng))
+
+    peryp = np.zeros((nput, ng))
+    ampyp = np.zeros((nput, ng))
+    fazyp = np.zeros((nput, ng))
+
     for i in range(nput):
         for j in range(ng):
-            perut[i][j] = 1 / ff[]
+            perut[i, j] = 1 / (ff[j] * 1000)
+            amput[i, j] = ff[j] * 100
+            fazut[i, j] = ff[j]
+            perxp[i, j] = 1 / (ff[j] * 10000)
+            ampxp[i, j] = ff[j] * 100
+            fazxp[i, j] = ff[j]
+
+    peryp = perxp
+    ampyp = ampxp
+    fazyp = fazxp
+
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from Moduls import ParamReader
 
 if __name__ == "__main__":
-    print(ParamReader.rdk())
+    print(ParamReader.DecodeParam2())
